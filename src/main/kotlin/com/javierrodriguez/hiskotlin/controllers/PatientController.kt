3c77be503bc346e1974ad66bd6b0a6a2dc0df068
Xavi <xavi@geekshubs.com>
2019-11-04 13:21:31 +0100

    implements PatientController add PatientService

--- src/main/kotlin/com/javierrodriguez/hiskotlin/controllers/PatientController.kt
+++ src/main/kotlin/com/javierrodriguez/hiskotlin/controllers/PatientController.kt
@@ -1,8 +1,12 @@
 package com.javierrodriguez.hiskotlin.controllers
 
+import com.javierrodriguez.hiskotlin.domain.entities.Patient
+import com.javierrodriguez.hiskotlin.domain.services.IPatientService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/v1")
 class PatientController {
 
+    @Autowired
+    private lateinit var patientService:IPatientService
+
     @RequestMapping("hello")
     fun HelloWorld(): ResponseEntity<Unit> {
      return ResponseEntity<Unit>(HttpStatus.OK);
     }
+
+    @RequestMapping("patients","GET","application/json")
+    /*fun getPatients():ResponseEntity<List<Patient>>{
+        val list:List<Patient> = patientService.getPatients()
+        return ResponseEntity(list, HttpStatus.OK)
+    }*/
+    fun getPatients():ResponseEntity<List<Patient>> = ResponseEntity(patientService.getPatients(), HttpStatus.OK)
 }
