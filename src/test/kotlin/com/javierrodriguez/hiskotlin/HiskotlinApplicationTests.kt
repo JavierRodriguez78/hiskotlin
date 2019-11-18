0299f28e2552dabb7c6ae70b5bb3aed12d26e7f6
Xavi <xavi@geekshubs.com>
2019-11-18 14:10:07 +0100

    modified to test

--- src/test/kotlin/com/javierrodriguez/hiskotlin/HiskotlinApplicationTests.kt
+++ src/test/kotlin/com/javierrodriguez/hiskotlin/HiskotlinApplicationTests.kt
@@ -1,17 +1,17 @@
 package com.javierrodriguez.hiskotlin
 
-import org.junit.jupiter.api.Test
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.junit4.SpringRunner
-@RunWith(SpringJUnit4ClassRunner::class)
-@SpringBootTest(classes = arrayOf(HiskotlinApplication::class), webEnvironment = WebEnvironment.RANDOM_PORT)
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
 class HiskotlinApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun `This application's context should successfully load ❤️`() {
     }
+
 
 }
--- src/test/kotlin/com/javierrodriguez/hiskotlin/controllers/PatientControllersTest.kt
+++ src/test/kotlin/com/javierrodriguez/hiskotlin/controllers/PatientControllersTest.kt
@@ -1,14 +1,46 @@
 package com.javierrodriguez.hiskotlin.controllers
 
 import com.javierrodriguez.hiskotlin.HiskotlinApplication
+import com.javierrodriguez.hiskotlin.patients.domain.entities.Patient
+import com.javierrodriguez.hiskotlin.patients.infraestructure.controllers.PatientController
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit4.SpringRunner
-
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import javax.swing.Spring
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(classes = arrayOf(HiskotlinApplication::class), webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(PatientController::class)
 class PatientControllersTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+
+
+
+
+    @Test
+    fun returnForbiddenIsNotAuthenticated():Unit {
+        mockMvc.perform(get("/patients")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andDo(print())
+                .andExpect(status().isOk)
+
+    }
 }
